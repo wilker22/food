51fd0ff6a29ebf3b8fff17bd460531ca576586a6
wilker22 <wilker.silva@codevasf.gov.br>
2024-11-29 16:19:04 -0300

    login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -77,4 +77,30 @@ class AdminController extends Controller
 
         return redirect()->back()->with('success', 'Reset Password link send to your e-mail');
     }
+
+    public function AdminResetPassword()
+    {
+        $admin_data = Admin::where('email', $email)->where('token', $token)->first();
+
+        if (!$admin_data) {
+            return redirect()->route('admin.login')->with('error', 'InvalidToken or email');
+        }
+
+        return view('admin.reset_passord', compact('token', 'email'));
+    }
+
+    public function AdminResetPasswordSubmit(Request $request)
+    {
+        $request->validate([
+            'password' => 'required',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        $admin_data = Admin::where('email', $request->email)->where('token', $request->token)->first();
+        $admin_data->password =  Hash::make($request->password);
+        $admin_data->token = "";
+        $admin_data->update();
+
+        return redirect()->route('admin.login')->with('success', 'Password Reset successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::post('/admin/login_submit', [AdminController::class, 'AdminLoginSubmit'])
 Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 Route::get('/admin/forget_password', [AdminController::class, 'AdminForgetPassword'])->name('admin.forget_password');
 Route::post('/admin/password_submit', [AdminController::class, 'AdminPasswordSubmit'])->name('admin.password_submit');
+Route::get('/admin/reset_password{token}/{email}', [AdminController::class, 'AdminResetPassword']);
+Route::post('/admin/reset_password_submit', [AdminController::class, 'AdminResetPasswordSubmit'])->name('admin.reset_password_submit');
